6402c2d022e073b89ae0656c07ea452f7ca360fd
Marcindev03 <marcindev03@gmail.com>
2021-05-18 13:18:17 +0200

    add page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
+import { motion, AnimatePresence } from 'framer-motion';
+
 import Layout from '../components/Layout/Layout';
 
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
+const variants = {
+  pageInitial: {
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  pageAnimate: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={variants}
+      >
+        <Component {...pageProps} />
+      </motion.div>
     </Layout>
   );
 }
